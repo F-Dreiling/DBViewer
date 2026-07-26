95b07f664da94f24454d27d62cd6d69c80dcf346
FX Drooklyn <dr.f380@hotmail.com>
2026-07-26 21:10:30 +0200

    basepath handling for different deployments

--- index.php
+++ index.php
@@ -1,35 +1,10 @@
 <?php
     session_start();
 
-    /*
-    |--------------------------------------------------------------------------
-    | Static file handling for Choreo
-    |--------------------------------------------------------------------------
-    */
+    $basePath = rtrim( dirname($_SERVER['SCRIPT_NAME']), '/\\' );
 
-    $uri = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
-    $uri = trim($uri, '/');
-
-    if ( str_starts_with($uri, 'css/') || str_starts_with($uri, 'js/') ) {
-        $file = __DIR__ . '/' . $uri;
-
-        if (file_exists($file)) {
-
-            $mimeTypes = [
-                'css' => 'text/css',
-                'js'  => 'application/javascript'
-            ];
-
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-
-            header(
-                'Content-Type: ' .
-                ($mimeTypes[$extension] ?? 'application/octet-stream')
-            );
-
-            readfile($file);
-            exit;
-        }
+    if ( $basePath === '/' ) {
+        $basePath = '';
     }
 
     require_once 'includes/functions.php';
@@ -51,8 +26,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>let sessionData = <?php echo json_encode($_SESSION); ?>;</script>
-    <script src="/js/actions.js"></script>
-    <link rel="stylesheet" href="/css/styles.css">
+    <script src="<?= $basePath ?>/js/actions.js"></script>
+    <link rel="stylesheet" href="<?= $basePath ?>/css/styles.css">
     <title>DBViewer</title>
 </head>
 <body>
